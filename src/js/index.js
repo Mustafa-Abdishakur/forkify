213b0f6d9ca0449b03310370fb0241f20ca7e56b
mustafa abdishakur <mx_1200@hotmail.com>
2020-06-26 10:14:26 +0400

    spelling error fix

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import * as likesView from './views/likesView';
 import {elements, renderLoader,clearLoader} from './views/base';
 import Recipe from './models/Recipe';
 import List from './models/List';
-import Likes from './models/likes';
+import Likes from './models/Likes';
 
 
 /** Global state of the app
